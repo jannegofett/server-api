ec05cee856372c776f391b3b3fb24462ce94fe48
jannegofett <jannegofett@gmail.com>
2016-06-12 03:14:07 +0800

    git handlebars

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+// MORE IMPORTS HERE
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+   res.render('home');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use(function (req, res, next) {
+   console.log("Looking for URL : " + req.url);
+   next();
+});
+
+app.get('/junk', function(req, res, next){
+   console.log('Tried to access /junk');
+   throw new Error('/junk doesn\'t exists');
+});
+
+app.use(function(err, req, res, next){
+   console.log('Error : ' + err.message);
+   next();
+});
+
+
+app.get('/about', function(req, res){
+   res.render('about');
+});
+
+
+
+
+app.listen(app.get('port'), function(){
+   console.log("Express started on http://localhost:" + app.get('port') + ' press Ctrl-C to terminate');
+
 });
